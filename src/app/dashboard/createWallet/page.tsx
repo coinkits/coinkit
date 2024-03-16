abec2cbe17adb02ed3dc3fcf3c29ccd3857d3d4d
coinkit <coin9520@gmail.com>
2024-03-16 11:07:28 +0800

    init

---
+++ src/app/dashboard/createWallet/page.tsx
@@ -0,0 +1,66 @@
+'use client';
+import * as React from 'react';
+import {
+  Button,
+  Paper,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
+import * as ethers from 'ethers';
+
+interface Wallet {
+  address: string;
+  privateKey: string;
+  mnemonic?: string;
+}
+export default function Page(): React.JSX.Element {
+  const [wallet, setWallet] = React.useState<Wallet[]>([]);
+  const createWallet = () => {
+    const { address, privateKey, mnemonic } = ethers.Wallet.createRandom();
+    setWallet([{ address, privateKey, mnemonic: mnemonic?.phrase }, ...wallet]);
+    console.warn({ address, privateKey, mnemonic: mnemonic?.phrase });
+  };
+  return (
+    <Stack spacing={3}>
+      <div>
+        <Typography variant="h4">Create Wallet</Typography>
+      </div>
+      <Grid container spacing={3}>
+        <Grid lg={4} md={6} xs={12}>
+          <Button variant="contained" onClick={createWallet}>
+            创建钱包
+          </Button>
+        </Grid>
+      </Grid>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align='left'>address</TableCell>
+              <TableCell align='left'>privateKey</TableCell>
+              <TableCell align='left'>mnemonic</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {wallet.map((row) => (
+              <TableRow key={row.address} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                <TableCell component="th" scope="row">
+                  {row.address}
+                </TableCell>
+                <TableCell align="right">{row.privateKey}</TableCell>
+                <TableCell align="right">{row.mnemonic}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Stack>
+  );
+}
---
+++ wallet.js
@@ -0,0 +1,6 @@
+const wallet = {
+  address: '0xFFCFf33C74F9Ff518db0C5eB736e77d6D5447c85',
+  privateKey: '0x21fefd748e9727eea00d07111142ea7f06fc8c1552d7ebb3536151f97a201d87',
+  mnemonic: 'during wife wire february test found mention pact session author pill staff',
+  pass: '21871283',
+};
